8eaf057555d8a128614e82159ac462f8b63ae064
tskim <tskim@MacBook-Pro.local>
2026-01-02 22:24:12 +0900

    Move diectory

--- leatcode/top_interview_questions/easy/contains_duplicate/solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) != len(nums)
-
-if __name__ == "__main__":
-    nums = [1,2,3,1]
-    print(Solution().containsDuplicate(nums))
